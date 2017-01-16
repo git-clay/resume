6ab68260293f190d5d0b44419e386a4c648c16fc
git-clay <ce.smith5525@gmail.com>
2017-01-16 00:03:35 -0700

    lots

---
+++ routes/routes.js
@@ -0,0 +1,38 @@
+var express = require('express'),
+	router = express.Router(),
+	bodyParser = require('body-parser'),
+	methodOverride = require('method-override'),
+	passport = require('passport'),
+	userController = require('../controllers/userController.js'),
+	staticController = require('../controllers/static.js');
+
+
+
+router.route('/').get(staticController.home); //home
+
+/****************** Routes for authentication ************************/
+router
+  .get('/register',userController.getRegister)
+  .post('/users',userController.postRegister);
+
+router.get('/users',userController.getUsers);
+
+router.route('/login')
+  .get(userController.getLogin)
+  .post(userController.postLogin);
+
+router.route('/questionaire')
+	.get(userController.getQuestionaire)
+	.post(userController.postQuestionaire);
+	//.put?????
+router.route('/apis').get(function(req,res){
+res.render('apis.ejs');
+});
+
+router.route("/logout")
+  .get(userController.getLogout);
+
+/****************** Routes for api ************************/
+
+
+module.exports = router;
